7bda3412d3a879969caeeb86c0ec19ea7b7c6502
Cecilia Nilsson <cn223dw@student.lnu.se>
2025-12-23 11:42:58 +0100

    fix: auth

--- services/auth/src/middlewares/auth.js
+++ services/auth/src/middlewares/auth.js
@@ -10,7 +10,7 @@ import http from 'node:http'
 import { JsonWebToken } from '../lib/JsonWebToken.js'
 
 // Läs public key från fil
-const jwtPublicKey = fs.readFileSync('/app/keys/public.pem', 'utf8')
+const jwtPublicKey = process.env.JWT_PUBLIC_PEM
 
 export const authenticateJWT = async (req, res, next) => {
   try {
