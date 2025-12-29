9f048715bcc3df0af12fe4592d44714c3e02ddc9
as228gn <greblats_@hotmail.com>
2025-12-29 20:57:11 +0100

    fix: new try with correct url

--- services/auth/src/app.js
+++ services/auth/src/app.js
@@ -13,7 +13,7 @@ export const createApp = () => {
   const app = express()
 
   app.use(cors({
-  origin: 'https://lihag.194.47.171.149.nip.io', // frontend URL
+  origin: ['http://lihag.194.47.171.149.nip.io', 'http://lihag.admin.194.47.171.149.nip.io'], // frontend URL
   credentials: true, // om du skickar cookies eller auth headers
 }))
   app.use(express.json())
