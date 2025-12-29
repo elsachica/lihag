f21de9635ebdc368ccf163459bcd306ec9957839
as228gn <greblats_@hotmail.com>
2025-12-29 21:16:09 +0100

    new try with cors

--- services/auth/src/app.js
+++ services/auth/src/app.js
@@ -13,8 +13,9 @@ export const createApp = () => {
   const app = express()
 
   app.use(cors({
-  origin: ['http://lihag.194.47.171.149.nip.io', 'http://lihag.admin.194.47.171.149.nip.io'] // om du skickar cookies eller auth headers
-}))
+    origin: ['http://lihag.194.47.171.149.nip.io', 'http://lihag.admin.194.47.171.149.nip.io'],
+    credentials: true // om du skickar cookies eller auth headers
+  }))
   app.use(express.json())
   app.use(httpContext.middleware)
 
